be95eef7822e3bdf1239ae0a7107296e15a5891f
Bigfish <Bigfish3656@gmail.com>
2022-12-12 02:32:48 -1200

    Javascript with bugs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let addbtn = document.getElementById('addbtn')
 let clearbtn = document.getElementById('clearbtn')
 let todo_ol = document.getElementById('mylist')
+let inputitem = document.getElementById('inputItem')
+
 
 var todolistarray = [];
 
@@ -11,5 +13,35 @@ var clearlist = function(){
 }
 
 clearbtn.addEventListener('click',clearlist);
+
+
+var listItemObj = function(){
+
+}
+
+var addlist = function(){
+    this.content = '';
+    this.status = ' Incomplete';
+}
+let refreshloacalstorage = function(){
+    var todo = todolistarray;
+    localStorage.removeItem('mylist');
+    loacalStorage.setlistItem('mylist',JSON.stringify(todo));
+}
+
+var addlist = function(){
+    let newlistItem = new listItemObj();
+    newlistItem.content = inputitem.value;
+    todolistarray.push(newlistItem);
+
+    //Refresh
+    refreshloacalstorage();
+    var newitem = createItemDom(inputitem.value,'incomplete');
+    mylist.appendChild(newitem);
+    inputitem.value = '';
+}
+
+clearbtn.addeEventListener('click',clearlist)
+
 
 
